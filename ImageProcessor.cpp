2da6541626ddcbf315e1e15f2ea4daed7f1041b0
Carlos Delfino - Vaio <consultoria@carlosdelfino.eti.br>
2016-10-11 23:21:02 -0300

    adotado o uso da classe de configuração
    Com o uso da classe de configuração menor número de parametros são
    passados pelos métodos.

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -1,74 +1,108 @@
 #include"ImageProcessor.hpp"
 
+ImageProcessor::ImageProcessor(CycloConfig* config) {
+	setCropArea(config->getCropArea());
+	setPerspective(							//.
+			config->GetPerspectivePos(0),	//.
+			config->GetPerspectivePos(1),			//.
+			config->GetPerspectivePos(2),			//.
+			config->GetPerspectivePos(3)			//.
+					);
+	setInterestArea(config->getInterestArea());
+
+}
 ImageProcessor::ImageProcessor() {
 //	this->lambda = new cv::Mat(2, 4, CV_32FC1);
 //	this->inv_lambda = new cv::Mat(2, 4, CV_32FC1);
 }
-ImageProcessor::~ImageProcessor() {}
-	
+ImageProcessor::~ImageProcessor() {
+}
+
 cv::Mat ImageProcessor::AcquireForeground(cv::Mat &frame) {
-		cv::Mat fore;
+	cv::Mat fore;
 	cv::Mat greyImg;
 	//cvtColor( frame, frame, CV_BGR2GRAY );
 	//equalizeHist( frame, frame );
-		this->bg(frame, fore);
-		cv::erode(fore, fore, cv::Mat());
-		return fore;
+	this->bg(frame, fore);
+	cv::erode(fore, fore, cv::Mat());
+	return fore;
 }
-	
+
 void ImageProcessor::PrintText(cv::Mat &frame, std::string text,
-							   cv::Point position) {
-	cv::putText(frame, text,  position, CV_FONT_HERSHEY_PLAIN, 1,
-				cv::Scalar(0, 0, 0));
+		cv::Point position) {
+	cv::putText(frame, text, position, CV_FONT_HERSHEY_PLAIN, 1,
+			cv::Scalar(0, 0, 0));
 }
-	
-void ImageProcessor::InsertInterestArea(cv::Mat &frame,cv::Rect interestArea) {
+
+void ImageProcessor::InsertInterestArea(cv::Mat &frame) {
+	InsertInterestArea(frame, interestArea);
+}
+
+void ImageProcessor::InsertInterestArea(cv::Mat &frame, cv::Rect interestArea) {
 	cv::rectangle(frame, interestArea, cv::Scalar(0, 0, 0), 1);
 }
 
-void ImageProcessor::PrepareFrame(cv::Mat &frame, 
-		  cv::Rect cropArea, 
-		  cv::Point2f p0, 
-		  cv::Point2f p1, 
-		  cv::Point2f p2, 
-		  cv::Point2f p3) {
+void ImageProcessor::PrepareFrame(cv::Mat &frame) {
+	PrepareFrame(frame, cropArea, quad[0], quad[1], quad[2], quad[3]);
+}
+
+void ImageProcessor::PrepareFrame(cv::Mat &frame, cv::Rect cropArea,
+		cv::Point2f p0, cv::Point2f p1, cv::Point2f p2, cv::Point2f p3) {
 	//this->RotateImage(frame);
 	this->PerspectiveTransform(frame, p0, p1, p2, p3);
 	this->CropImage(frame, cropArea);
 }
 
+void ImageProcessor::setCropArea(cv::Rect cropArea) {
+	this->cropArea = cropArea;
+}
+
+void ImageProcessor::setInterestArea(cv::Rect interestArea) {
+	this->interestArea = interestArea;
+}
+void ImageProcessor::setPerspective(	//.
+		cv::Point2f p0,					//.
+		cv::Point2f p1, 					//.
+		cv::Point2f p2, 					//.
+		cv::Point2f p3					//.
+		) {
+	quad[0] = p0;
+	quad[1] = p1;
+	quad[2] = p2;
+	quad[3] = p3;
+}
 
 void ImageProcessor::RotateImage(cv::Mat &frame) {
 	cv::transpose(frame, frame);
-	cv::flip(frame, frame, 0);		
-	cv::flip(frame, frame, 1);		
+	cv::flip(frame, frame, 0);
+	cv::flip(frame, frame, 1);
 }
 
-void ImageProcessor::PerspectiveTransform(cv::Mat &frame, 
-			  cv::Point2f p0, 
-			  cv::Point2f p1, 
-			  cv::Point2f p2, 
-			  cv::Point2f p3) {
-				  
-	cv::Point2f inputQuad[4]; 
+void ImageProcessor::PerspectiveTransform(cv::Mat &frame, cv::Point2f p0,
+		cv::Point2f p1, cv::Point2f p2, cv::Point2f p3) {
+
+	cv::Point2f inputQuad[4];
 	cv::Point2f outputQuad[4];
-	
+
 	inputQuad[0] = p0;
 	inputQuad[1] = p1;
 	inputQuad[2] = p2;
-	inputQuad[3] = p3;			
-			
-	int x0d, x1d, x2d, x3d, y0d, y1d, y2d, y3d;		 
-	x0d=0;    y0d=0;
-	x1d=p1.x; y1d=0;
-	x2d=0;    y2d=std::max(p2.y - p0.y, p3.y - p1.y);
-	x3d=p3.x; y3d=std::max(p2.y -p0.y, p3.y - p1.y);
-	
+	inputQuad[3] = p3;
+
+	int x0d, x1d, x2d, x3d, y0d, y1d, y2d, y3d;
+	x0d = 0;
+	y0d = 0;
+	x1d = p1.x;
+	y1d = 0;
+	x2d = 0;
+	y2d = std::max(p2.y - p0.y, p3.y - p1.y);
+	x3d = p3.x;
+	y3d = std::max(p2.y - p0.y, p3.y - p1.y);
+
 	outputQuad[0] = cv::Point2f(x0d, y0d);
 	outputQuad[1] = cv::Point2f(x1d, y1d);
 	outputQuad[2] = cv::Point2f(x2d, y2d);
 	outputQuad[3] = cv::Point2f(x3d, y3d);
-
 
 	// Get the Perspective Transform Matrix i.e. lambda
 	this->lambda = cv::getPerspectiveTransform(inputQuad, outputQuad);
@@ -78,5 +112,5 @@ void ImageProcessor::PerspectiveTransform(cv::Mat &frame,
 }
 
 void ImageProcessor::CropImage(cv::Mat &frame, cv::Rect cropArea) {
-		frame = frame(cropArea);
+	frame = frame(cropArea);
 }
--- ImageProcessor.hpp
+++ ImageProcessor.hpp
@@ -4,32 +4,39 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
 
+#include "CycloConfig.hpp"
+
 class ImageProcessor {
 private:
+	CycloConfig *config;
 	cv::BackgroundSubtractorMOG2 bg;
 	cv::Mat lambda; //used to get changes in perspective
 	cv::Mat frame;
+	cv::Rect cropArea;
+	cv::Point2f quad[4];
+	cv::Rect interestArea;
 public:
-	ImageProcessor();	
-	~ImageProcessor();	
-	cv::Mat AcquireForeground(cv::Mat &frame);	
+	ImageProcessor();
+	ImageProcessor(CycloConfig* config);
+	~ImageProcessor();
+	cv::Mat AcquireForeground(cv::Mat &frame);
 	void PrintText(cv::Mat &frame, std::string text, cv::Point position);
-	void InsertInterestArea(cv::Mat &frame,  cv::Rect interestArea);
-	void PrepareFrame(cv::Mat &frame, 
-					  cv::Rect cropArea, 
-					  cv::Point2f p0, 
-					  cv::Point2f p1, 
-					  cv::Point2f p2, 
-					  cv::Point2f p3);
-	  
+	void InsertInterestArea(cv::Mat &frame);
+	void InsertInterestArea(cv::Mat &frame, cv::Rect interestArea);
+	void PrepareFrame(cv::Mat &frame);
+	void PrepareFrame(cv::Mat &frame, cv::Rect cropArea, cv::Point2f p0,
+			cv::Point2f p1, cv::Point2f p2, cv::Point2f p3);
+
 	void RotateImage(cv::Mat &frame);
+
+	void setCropArea(cv::Rect cropArea);
+	void setInterestArea(cv::Rect interestArea);
+	void setPerspective(cv::Point2f p0, cv::Point2f p1, cv::Point2f p2,
+			cv::Point2f p3);
 protected:
-	void PerspectiveTransform(cv::Mat &frame, 
-							  cv::Point2f p0, 
-							  cv::Point2f p1, 
-							  cv::Point2f p2, 
-							  cv::Point2f p3);
-	
+	void PerspectiveTransform(cv::Mat &frame, cv::Point2f p0, cv::Point2f p1,
+			cv::Point2f p2, cv::Point2f p3);
+
 	void CropImage(cv::Mat &frame, cv::Rect cropArea);
 };
 #endif
